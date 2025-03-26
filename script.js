6806b234f8970897858dbb48e0de7aacb28c85dc
AnuragBaruah47 <anuragbaruah2004@gmail.com>
2025-03-27 03:02:34 +0530

    Round Update Done

--- script.js
+++ script.js
@@ -12,8 +12,6 @@ const button7=document.getElementById("7")
 const button8=document.getElementById("8")
 const button9=document.getElementById("9")
 let randomNumber;
-console.log(button1);
-
 let patterns = [];
 let game = false;
 //buttons ka logic sath mei main game ka bhi(logic of my buttons with the main game)
@@ -22,127 +20,33 @@ startButton.addEventListener("click", () => {
   if (game === true) {
     randomNumber =  (Math.random() * 8 + 1).toFixed(0);
     patterns.push(randomNumber);
-    if (randomNumber==1) {
-      button1.style.backgroundColor="#FFFFFF"
-      setTimeout(()=>{
-        button1.style.backgroundColor="#73A5C6"
-      },500)
+    if (patterns.length>0) {
+      for (let i = 0; i < patterns.length; i++) {
+        const element = i+1;
+        if (element) {
+             roundUpdateInfo.innerText=`Round ${element}`
+        }
+      }
     }
-    if (randomNumber==2) {
-      setTimeout(()=>{
-        button2.style.backgroundColor="#73A5C6"
-      },500)
-      button2.style.backgroundColor="#FFFFFF"
-    }
-    if (randomNumber==3) {
-      setTimeout(()=>{
-        button3.style.backgroundColor="#73A5C6"
-      },500)
-      button3.style.backgroundColor="#FFFFFF"
-    }
-    if (randomNumber==4) {
-      setTimeout(()=>{
-        button4.style.backgroundColor="#73A5C6"
-      },500)
-      button4.style.backgroundColor="#FFFFFF"
-    }
-    if (randomNumber==5) {
-      setTimeout(()=>{
-        button5.style.backgroundColor="#73A5C6"
-      },500)
-      button5.style.backgroundColor="#FFFFFF"
-    }
-    if (randomNumber==6) {
-      setTimeout(()=>{
-        button6.style.backgroundColor="#73A5C6"
-      },500)
-      button6.style.backgroundColor="#FFFFFF"
-    }
-    if (randomNumber==7) {
-      setTimeout(()=>{
-        button7.style.backgroundColor="#73A5C6"
-      },500)
-      button7.style.backgroundColor="#FFFFFF"
-    }
-    if (randomNumber==8) {
-      setTimeout(()=>{
-        button8.style.backgroundColor="#73A5C6"
-      },500)
-      button8.style.backgroundColor="#FFFFFF"
-    }
-    if (randomNumber==9) {
-      setTimeout(()=>{
-        button9.style.backgroundColor="#73A5C6"
-      },500)
-      button9.style.backgroundColor="#FFFFFF"
-    }
+    console.log(patterns);
     buttons.forEach((button)=>{
       button.addEventListener("click",()=>{
         randomNumber =(Math.random() * 8 + 1).toFixed(0);
         patterns.push(randomNumber);
-        if (randomNumber==1) {
-          button1.style.backgroundColor="#FFFFFF"
-          setTimeout(()=>{
-            button1.style.backgroundColor="#73A5C6"
-          },500)
+        if (patterns.length>0) {
+          for (let i = 0; i < patterns.length; i++) {
+            const element = i+1;
+            if (element) {
+                 roundUpdateInfo.innerText=`Round ${element}`
+            }
+          }
         }
-        if (randomNumber==2) {
-          setTimeout(()=>{
-            button2.style.backgroundColor="#73A5C6"
-          },500)
-          button2.style.backgroundColor="#FFFFFF"
-        }
-        if (randomNumber==3) {
-          setTimeout(()=>{
-            button3.style.backgroundColor="#73A5C6"
-          },500)
-          button3.style.backgroundColor="#FFFFFF"
-        }
-        if (randomNumber==4) {
-          setTimeout(()=>{
-            button4.style.backgroundColor="#73A5C6"
-          },500)
-          button4.style.backgroundColor="#FFFFFF"
-        }
-        if (randomNumber==5) {
-          setTimeout(()=>{
-            button5.style.backgroundColor="#73A5C6"
-          },500)
-          button5.style.backgroundColor="#FFFFFF"
-        }
-        if (randomNumber==6) {
-          setTimeout(()=>{
-            button6.style.backgroundColor="#73A5C6"
-          },500)
-          button6.style.backgroundColor="#FFFFFF"
-        }
-        if (randomNumber==7) {
-          setTimeout(()=>{
-            button7.style.backgroundColor="#73A5C6"
-          },500)
-          button7.style.backgroundColor="#FFFFFF"
-        }
-        if (randomNumber==8) {
-          setTimeout(()=>{
-            button8.style.backgroundColor="#73A5C6"
-          },500)
-          button8.style.backgroundColor="#FFFFFF"
-        }
-        if (randomNumber==9) {
-          setTimeout(()=>{
-            button9.style.backgroundColor="#73A5C6"
-          },500)
-          button9.style.backgroundColor="#FFFFFF"
-        }
-        console.log(randomNumber);
-        console.log(patterns);
       })
     })
 }
 });
 function start() {
   game = true;
-   roundUpdateInfo.innerText="Round 1"
    startButton.disabled=true
 }
 function stop() {
@@ -150,4 +54,8 @@ function stop() {
   game = false;
   roundUpdateInfo.innerText="Game Over"
   startButton.disabled=false
+}
+
+function rounds(){
+
 }
