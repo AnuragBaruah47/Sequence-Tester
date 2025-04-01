7f9b53724bb13fb1998dbcfbe3c5187aa0c65007
AnuragBaruah47 <anuragbaruah2004@gmail.com>
2025-04-02 02:27:46 +0530

    Function almost ready

--- script.js
+++ script.js
@@ -2,60 +2,51 @@ const buttons = document.querySelectorAll(".numbuttons");
 const startButton = document.querySelector(".startbutton");
 const roundUpdateInfo = document.querySelector(".title");
 const body=document.querySelector("body")
-const button1=document.getElementById("1")
-const button2=document.getElementById("2")
-const button3=document.getElementById("3")
-const button4=document.getElementById("4")
-const button5=document.getElementById("5")
-const button6=document.getElementById("6")
-const button7=document.getElementById("7")
-const button8=document.getElementById("8")
-const button9=document.getElementById("9")
-let randomNumber;
-let patterns = [];
-let game = false;
-//buttons ka logic sath mei main game ka bhi(logic of my buttons with the main game)
-startButton.addEventListener("click", () => {
-  start();
-  if (game === true) {
-    randomNumber =  (Math.random() * 8 + 1).toFixed(0);
-    patterns.push(randomNumber);
-    if (patterns.length>0) {
-      for (let i = 0; i < patterns.length; i++) {
-        const element = i+1;
-        if (element) {
-             roundUpdateInfo.innerText=`Round ${element}`
-        }
-      }
-    }
-    console.log(patterns);
-    buttons.forEach((button)=>{
-      button.addEventListener("click",()=>{
-        randomNumber =(Math.random() * 8 + 1).toFixed(0);
-        patterns.push(randomNumber);
-        if (patterns.length>0) {
-          for (let i = 0; i < patterns.length; i++) {
-            const element = i+1;
-            if (element) {
-                 roundUpdateInfo.innerText=`Round ${element}`
-            }
-          }
-        }
-      })
+let round=0
+let game=false
+let patterns=[]
+let buttonId=[]
+let allButtonId=[1,2,3,4,5,6,7,8,9]
+let randomNumber=(Math.random() * 8 + 1).toFixed(0)
+
+startButton.addEventListener("click",()=>{
+ gameStart()
+})
+const mainGame=()=>{
+  buttons.forEach((button)=>{
+    button.addEventListener("click",function(){
+
     })
+  })
 }
-});
-function start() {
-  game = true;
-   startButton.disabled=true
+ 
+const gameStart=()=>{
+  round++
+  startButton.disabled=true
+  return game=true
 }
-function stop() {
-  patterns = [];
-  game = false;
-  roundUpdateInfo.innerText="Game Over"
+const buttonBlink=()=>{
+  loop()
+  setTimeout(()=>{
+    if (loopNumber==randomNumber) {
+       button
+    }
+  },300)
+}
+ 
+const gameOver=()=>{
   startButton.disabled=false
+  patterns=[]
+  buttonId=[]
+  return game=false
 }
 
-function rounds(){
-
+function loop(){
+  for (let loopNumber = 0; loopNumber <=9; i++) {
+    return loopNumber
+  }
 }
+
+
+
+
