e5c07a4119589221bbbc4a0556df16f196b12aef
AnuragBaruah47 <anuragbaruah2004@gmail.com>
2025-04-07 17:02:44 +0530

    Css added with responsiveness for the score updater

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-const allButtons=document.querySelectorAll(".numbuttons")
-const body=document.querySelector("body")
-const titlecontainer=document.querySelector(".title")
-const totalScore=document.querySelector(".score")
-let game=false
-let round=0
-let userInput=[]
-let pattersArray=[]
-let randomNumber=(((Math.random()*8)+1)).toFixed(0)
-console.log(randomNumber);
+
+
+
